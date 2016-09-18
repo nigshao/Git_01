d375d67a71c62e99ba50e2aadb811aaecb665051
isshenhao <244217438@qq.com>
2016-09-18 20:59:39 +0800

    syosut1

---
+++ Git_01/src/com/atguigu/git/TestGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+public class TestGit {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("AA");
+		System.out.println("BB");
+		System.out.println("CC");
+		
+	}
+
+}
